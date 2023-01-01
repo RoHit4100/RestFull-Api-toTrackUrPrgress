d13e7f13dbc43bd792b9e3a048f2c37edbb0228b
RoHit4100 <rohitudamale777@gmail.com>
2023-01-01 18:16:07 +0530

    40 percent project is done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 main().catch(err => console.log(err));
 
@@ -10,6 +11,30 @@ async function main() {
     mongoose.set('strictQuery', true);
     await mongoose.connect('mongodb://127.0.0.1:27017/apiDb');
 }
+
+const articleSchema = mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            res.send(foundArticles);
+        })
+    })
+    .post(function (req, res) {
+        const title = req.body.title;
+        const content = req.body.content;
+        const article = new Article({
+            title: title,
+            content: content
+        });
+        article.save();
+        console.log(article);
+    })
 
 app.listen(port, function () {
     console.log("Server is running on port" + port);
